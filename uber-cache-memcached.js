34fa9c91de167a82841afa73a6f1b5edf6c53e83
Kuba Stawiarski <kuba.stawiarski@gmail.com>
2015-04-24 01:32:43 +0100

    Rewrite the engine to follow the same standards that other engines do
    * engine rewritten to work with uber-cache ^2.1.0
    * added jshint
    * tidied up package.json

---
+++ uber-cache-memcached.js
@@ -0,0 +1,142 @@
+module.exports = UberCacheMemcached
+
+var EventEmitter = require('events').EventEmitter
+  , through = require('through')
+  , noop = function () {}
+
+function UberCacheMemcached(memcached) {
+  this.memcached = memcached
+}
+
+UberCacheMemcached.prototype = Object.create(EventEmitter.prototype)
+
+function setCache(key, value, ttl, callback) {
+  var lifetime = ttl ? ttl : 0
+    , expiryTime = ttl ? ttl + Date.now() : undefined
+    , dataPacket =
+      { expiryTime: expiryTime
+      , data: value
+      , updated: new Date()
+      }
+
+  this.memcached.set(key, dataPacket, lifetime, function (error) {
+    if (typeof callback === 'function') {
+      // conformance test expects null rather than undefined
+      callback(error ? error : null, value)
+    }
+  })
+}
+
+UberCacheMemcached.prototype.set = function(key, value, ttl, callback) {
+  var stream
+
+  // If no TTL is defined then last as long as possible
+  if (typeof ttl === 'function') {
+    callback = ttl
+    ttl = undefined
+  }
+
+  // Don’t handle undefined cache keys
+  if (typeof key === 'undefined') {
+    return callback(new Error('Invalid key undefined'))
+  }
+
+  // Check for cyclic reference
+  try {
+    JSON.stringify(value)
+  } catch (e) {
+    return callback(e)
+  }
+
+  if ((value === undefined) && (callback === undefined)) {
+    value = []
+    return stream = through(function write(data) {
+        value.push(data)
+        this.queue(data)
+      }
+    ).on('end', (function () {
+        // Check for cyclic reference
+        try {
+          JSON.stringify(value)
+        } catch (e) {
+          return stream.emit('error', e)
+        }
+        setCache.call(this, key, value, ttl, (function(error) {
+          if (error) return this.emit('error', error)
+        }).bind(this))
+      }).bind(this))
+  }
+  setCache.call(this, key, value, ttl, callback)
+}
+
+UberCacheMemcached.prototype.get = function(key, callback) {
+  this.memcached.get(key, (function (error, cachePacket) {
+    var value
+
+    if (error) return callback(error)
+
+    if (!cachePacket) {
+      this.emit('miss', key)
+      return callback(null, null)
+    }
+
+    value = cachePacket.data
+    // If ttl has expired, delete
+    if (cachePacket.expiryTime < Date.now()) {
+      this.delete(key)
+      this.emit('miss', key)
+      this.emit('stale', key, value, cachePacket.expiryTime)
+      value = undefined
+    }
+
+    callback(null, value)
+  }).bind(this))
+}
+
+UberCacheMemcached.prototype.delete = function(key, callback) {
+  if (typeof callback !== 'function') callback = noop
+
+  this.memcached.delete(key, (function(error) {
+    if (error) return callback(error)
+    this.emit('delete', key)
+    callback(null)
+  }).bind(this))
+}
+
+UberCacheMemcached.prototype.clear = function(callback) {
+  if (typeof callback !== 'function') callback = noop
+  this.memcached.flush((function(err) {
+    if (err) return callback(err)
+    this.emit('clear')
+    callback()
+  }).bind(this))
+}
+
+/* This function parses the output from `stats items`, extracts slabid and then
+   calls `stats cachedump` for each of the slabs to get the items back.
+   It is therefore very slow and should not be used. It is here only to make
+   testing possible.
+*/
+UberCacheMemcached.prototype.size = function(callback) {
+
+  this.memcached.items((function (error, result) {
+    result.forEach((function (itemSet) {
+      var keys = Object.keys(itemSet);
+
+      if (keys.length === 0) return callback(undefined, 0)
+
+      keys.pop();
+      keys.forEach((function (slabId) {
+        this.memcached.cachedump(itemSet.server, parseInt(slabId, 10), itemSet[slabId].number
+            , function (err, items) {
+          if (error) return callback(error)
+
+          if (Array.isArray(items)) {
+            return callback(error, items.length)
+          }
+          return callback(error, 1)
+        })
+      }).bind(this))
+    }).bind(this))
+  }).bind(this))
+}
